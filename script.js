66b135db054803030730e3e7eda86affeed0948d
Ashtaa <96940778+Ashtaa@users.noreply.github.com>
2024-03-31 14:49:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function closeNav() {
   document.getElementById("mySidebar").style.left = "-200px";
   document.getElementsByClassName("content")[0].style.marginLeft = "0";
 }
+// back to top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // Scroll smoothly to the top
+  });
+}
+
+window.addEventListener("scroll", function() {
+  var backToTopBtn = document.getElementById("backToTopBtn");
+  
+  if (window.pageYOffset > 200) {
+    backToTopBtn.style.display = "block"; // Show the button when scrolling down 200px
+  } else {
+    backToTopBtn.style.display = "none"; // Hide the button when scrolling back to top
+  }
+});
